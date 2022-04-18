543991d7c38f1e13fa7882ef753d480d978badbb
huskyhehe <zhou.he1@northeastern.edu>
2022-04-18 01:59:29 -0700

    add meme generator

---
+++ meme-generator/src/App.jsx
@@ -0,0 +1,15 @@
+import React from "react"
+import Header from "./components/Header"
+
+/**
+ * Challenge: Build the Header component
+ */
+function App() {
+    return (
+        <div>
+            <Header />
+        </div>
+    );
+}
+
+export default App;
