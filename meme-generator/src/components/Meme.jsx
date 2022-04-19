f7156bbf058c15a7b6094d099403268903906089
huskyhehe <zhou.he1@northeastern.edu>
2022-04-18 17:29:51 -0700

    add meme component

--- meme-generator/src/components/Meme.jsx
+++ meme-generator/src/components/Meme.jsx
@@ -1,9 +1,23 @@
 import React from "react"
+import memesData from "./memesData.js"
 
 export default function Meme() {
+    /**
+     * Challenge: Get a random image from the `memesData` array
+     * when the "new meme image" button is clicked.
+     * 
+     * Log the URL of the image to the console. (Don't worry
+     * about displaying the image yet)
+     */
+    function getMemeImage() {
+        const memesArray = memesData.data.memes;
+        const randomNum = Math.floor(Math.random * memesArray.length)
+        const url = memesArray[randomNum].url
+    };
+
     return (
         <main>
-            <form className="form">
+            <div className="form">
                 <input 
                     type="text"
                     placeholder="Top text"
@@ -16,10 +30,11 @@ export default function Meme() {
                 />
                 <button 
                     className="form--button"
+                    onClick={getMemeImage}
                 >
                     Get a new meme image 🖼
                 </button>
-            </form>
+            </div>
         </main>
-    )
+    );
 }
